82643b6181dfa7ccbe08f450bec890e3eef4f31a
kikspine <kikspine@gmail.com>
2021-11-23 12:50:41 +0400

    add fullscreen

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 const constrains = {
-    audio: false,
-    video: {
-        width: { ideal: 1920 },
-        height: { ideal: 1080 }
-    }
+  audio: false,
+  video: {
+    width: { ideal: 1920 },
+    height: { ideal: 1080 },
+  },
 }
 
-const initCamera = async () => {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia(constrains)
+const initCamera = async (video) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constrains)
+    video.srcObject = stream
+    video.play()
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-        const video = document.querySelector('.video')
-        video.srcObject = stream
-        video.play()
-    } catch (e) {
-        console.error(e)
-    }
-    
+const openFullscreen = (elem) => {
+    elem.requestFullscreen()
+
+    elem.addEventListener('mousedown', () => elem.exitFullscreen())
+    elem.addEventListener('keydown', () => elem.exitFullscreen())
 }
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
-            .then(res => console.log("service worker registered"))
-            .catch(err => console.log("service worker not registered", err))
-    })    
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then((res) => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err))
+  });
 }
 
 window.addEventListener('load', () => {
-    initCamera()
+    const video = document.querySelector('.video')
+    openFullscreen(video)
+    initCamera(video)
 })
