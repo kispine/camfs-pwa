ec00296db22eb40937f036fb9b55d5ed4aca2355
kikspine <kikspine@gmail.com>
2021-11-23 11:29:36 +0400

    base code

---
+++ main.js
@@ -0,0 +1,22 @@
+const constrains = {
+    audio: false,
+    video: {
+        width: { ideal: 1920 },
+        height: { ideal: 1080 }
+    }
+}
+
+const initCamera = async () => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constrains)
+
+        const video = document.querySelector('.video')
+        video.srcObject = stream
+        video.play()
+    } catch (e) {
+        console.error(e)
+    }
+    
+}
+
+window.onload = initCamera
